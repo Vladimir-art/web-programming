30c480c371de29235cdfaa014e57c6c23bd4a487
Vladimir Ermolaev <vladimir.ermolaev@mera.com>
2023-12-12 17:25:51 +0300

    created common css styles

--- js/carousel.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var myCarousel = document.getElementById('carouselExampleIndicators');
-    var carousel = new bootstrap.Carousel(myCarousel);
-    carousel();
-  });
